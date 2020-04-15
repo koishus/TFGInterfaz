6328e8479e4376e1324c3f65fc4019e95a62e683
koishus <andrea97uni@gmail.com>
2020-04-15 10:48:30 +0200

    pestaña style classification
    añadido el código encargado de manejar y clasificar por estilo artístico el input y mensaje de aviso cuando se le da al botón ya que faltan pesos

--- interfaz/apptfg.R
+++ interfaz/apptfg.R
@@ -14,7 +14,16 @@ ui <- fluidPage(
   
   # mainPanel(
     tabsetPanel(type='pills',
-                tabPanel("Style Classification"
+                tabPanel("Style Classification",
+                         sidebarPanel(
+                           fileInput("filepaint", "Load the image", accept = c('image/jpeg', 'image/png', 'image/jpg')),
+                           uiOutput("seleccionredpaint"),
+                           uiOutput("botoncargarpesopaint")
+                         ),
+                         mainPanel(
+                           plotOutput("imagencpaint"),
+                           uiOutput("resultadopaint")
+                         )
                          ),
                 tabPanel("Photograph or Painting Classification",
                          sidebarPanel(
@@ -37,7 +46,9 @@ ui <- fluidPage(
   )
 options(shiny.maxRequestSize=30*1024^2)
 server <- function(input, output, session) {
-  imagen <- NULL
+  imagenfoto <- NULL
+  imagenpaint <- NULL
+  
 
   #### elementos experimentos photo ####
   obtenerpathfoto <- reactive({
@@ -51,8 +62,8 @@ server <- function(input, output, session) {
     else
       # { print(gsub("\\\\", "/", arc$datapath))
     {
-      imagen <<- load.image(arc$datapath)
-      return(imagen)
+      imagenfoto <<- load.image(arc$datapath)
+      return(imagenfoto)
     }
   })
   
@@ -81,7 +92,7 @@ server <- function(input, output, session) {
                  # # img = load.image(path)
                  # 
                  # print(typeof(img))
-                 img <- imager::resize(imagen, 224,224)
+                 img <- imager::resize(imagenfoto, 224,224)
                  
                  imager::save.image(img, "1/2/picture.jpg")
                  
@@ -157,7 +168,7 @@ server <- function(input, output, session) {
   observeEvent(input$filefoto, 
                {
                  output$imagencfoto <- renderPlot({
-                   plot(imagen)
+                   plot(imagenfoto)
                    
                  })
                  
@@ -171,25 +182,131 @@ server <- function(input, output, session) {
   #### elementos experimentos estilos ####
   
  
+  obtenerpathpaint <- reactive({
+    # browser()
+    # if(is.null(input$file$datapath)){return()}
+    image <- NULL
+    arc <- input$filepaint
+    if(is.null(arc)) {
+      
+      return()}
+    else
+      # { print(gsub("\\\\", "/", arc$datapath))
+    {
+      imagenpaint <<- load.image(arc$datapath)
+      return(imagenpaint)
+    }
+  })
+  
+  output$seleccionredpaint <- renderUI({
+    if (is.null(obtenerpathpaint())) {return()}
+    selectInput("redpaint", "Choose the model to use", c("Simple Network", "VGG-16 Network", "ResNet-50 Network"))
+  })
+  
+  output$botoncargarpesopaint <- renderUI({
+    if (is.null(obtenerpathpaint())) {return()}
+    actionButton("botoncargapaint", "Classify the painting!")
+  })
+  
+  observeEvent(input$botoncargapaint,
+               { 
+                 
+                 # modelo <- loadModelpaint()
+                 # if(!dir.exists("1"))
+                 # {
+                 #   dir.create("1")
+                 #   dir.create("1/2")
+                 # }
+                 # # browser()
+                 # # img = obtenerpath()
+                 # # 
+                 # # # img = load.image(path)
+                 # # 
+                 # # print(typeof(img))
+                 # img <- imager::resize(imagenpaint, 224,224)
+                 # 
+                 # imager::save.image(img, "1/2/picture.jpg")
+                 # 
+                 # 
+                 # test <- flow_images_from_directory("1",
+                 #                                    target_size=c(224,224),
+                 #                                    batch_size = 32,
+                 #                                    class_mode = 'binary',
+                 #                                    shuffle = FALSE)
+                 # resfinal = NULL
+                 # res <- modelo %>%
+                 #   predict_generator(test, steps = 1)
+                 # 
+                 # # este if cambiarlo dependiendo del resultado obtenido, 16 clases, el valor irá de 0 a 15
+                 # # if (res <= 0.5)
+                 # #   resfinal = "painting"
+                 # # 
+                 # # else resfinal = "photograph"
+                
+                 output$resultadopaint <- renderUI({
+                   
+                   # strfinal <- HTML(paste("<font size=+2>Classified as a <b>", resfinal, "</b> <br> Obtained score <b>", round(res, 4), "</b></font>"))
+                   strfinal <- HTML(paste("<font size=+2> Work in progress!! UwU</font>"))
+                   
+                   strfinal
+                 })
+               }
+               
+               
+               
+  )
+  getModelpaint <- reactive({
+    if(is.null(input$botoncargapaint)) {return()}
+    input$botoncargapaint
+    isolate({
+      if (input$botoncargapaint == 0) {return()}
+      
+      modelo <- input$redpaint
+      
+      
+    })
+    return(modelo)
+  })
+  
+  loadModelpaint <- reactive({
+    if (is.null(getModelpaint())) {return()}
+    
+    modelo <- getModelpaint()
+    redneuronal <- NULL
+    if (modelo == "Simple Network")
+    {
+      ## cargar pesos red simple
+      # redneuronal <- keras::load_model_hdf5("pesos/estilos/simple")
+    }
+    else if (modelo == "VGG-16 Network")
+    {
+      # redneuronal <- keras::load_model_hdf5("pesos/estilos/vgg")
+    }
+    else
+    {
+      # redneuronal <- keras::load_model_hdf5("pesos/estilos/resnet")
+    }
+    
+    return(redneuronal)
+  })
   
 
+  observeEvent(input$filepaint, 
+               {
+                 output$imagencpaint <- renderPlot({
+                   plot(imagenpaint)
+                   
+                 })
+                 
+                 output$resultadopaint <- NULL
+               })
   
+  observeEvent(input$redpaint, 
+               {
+                 output$resultadopaint <- NULL
+               })
+
   
-  # output$imagencargada <- renderUI({
-  #   img <- obtenerpath()
-  #   if(is.null(img)) {
-  # 
-  #     output$imagen <- NULL
-  #     }
-  #   # 
-  #   # else{
-  #   #   
-  #   #   output$imagen <- renderPlot({
-  #   #     plot(img)
-  #   #     
-  #   #   })
-  #   # } 
-  #   })
   
 }
 
