7807cf73c3829e55921bef5edb6f6d908ad149ec
koishus <andrea97uni@gmail.com>
2020-05-13 20:32:39 +0200

    Implementación provisional neural style
    integración del neural style transfer en la interfaz, no sé por qué la imagen final no muestra título. añadidos también pesos para style transfer provisionales

--- interfaz/apptfg.R
+++ interfaz/apptfg.R
@@ -3,6 +3,10 @@ library(keras)
 library(reticulate)
 library(imager)
 library(shinythemes)
+library(purrr)
+library(R6)
+library(gridExtra)
+
 
 ui <- fluidPage(
   theme = shinythemes::shinytheme("yeti"),
@@ -41,7 +45,23 @@ ui <- fluidPage(
                            # verbatimTextOutput("modelosel"),
                            htmlOutput("resultadofoto")
                            
-                         ))
+                         )), 
+                tabPanel("Style Transfer",
+                         sidebarPanel(
+                           fileInput("filebase", "Load the image to transform", accept = c('image/jpeg', 'image/png', 'image/jpg')),
+                           fileInput("filestyle", "Load the style reference", accept = c('image/jpeg', 'image/png', 'image/jpg')),
+                           uiOutput("iteraciones"),
+                           uiOutput("botonempezartransfer")
+                         ),
+                         mainPanel(
+                          fluidRow(
+                            splitLayout(cellWidths = c("50%", "50%"), plotOutput("imgbase"), plotOutput("imgestilo"))
+                          ),
+                           
+                            
+                           plotOutput("imgstyle")
+                         )
+                         )
                 )
     
     
@@ -51,6 +71,125 @@ options(shiny.maxRequestSize=30*1024^2)
 server <- function(input, output, session) {
   imagenfoto <- NULL
   imagenpaint <- NULL
+  imagebase <- NULL
+  imagestyle <- NULL
+  
+  
+  #### funciones importadas de neural_style_transfer ####
+  total_variation_weight <- 1
+  style_weight <- 1
+  content_weight <- 0.025
+  
+  # util function to open, resize and format pictures into appropriate tensors
+  preprocess_image <- function(path){
+
+    img <- image_load(path, target_size = c(img_nrows, img_ncols)) %>%
+      image_to_array() %>%
+      array_reshape(c(1, dim(.)))
+    imagenet_preprocess_input(img)
+  }
+
+  # util function to convert a tensor into a valid image
+  # also turn BGR into RGB.
+  deprocess_image <- function(x){
+    x <- x[1,,,]
+    # Remove zero-center by mean pixel
+    x[,,1] <- x[,,1] + 103.939
+    x[,,2] <- x[,,2] + 116.779
+    x[,,3] <- x[,,3] + 123.68
+    # BGR -> RGB
+    x <- x[,,c(3,2,1)]
+    # clip to interval 0, 255
+    x[x > 255] <- 255
+    x[x < 0] <- 0
+    x[] <- as.integer(x)/255
+    x
+  }
+
+  # compute gram matrix of image tensor
+  gram_matrix <- function(x){
+
+    features <- x %>%
+      k_permute_dimensions(pattern = c(3, 1, 2)) %>%
+      k_batch_flatten()
+
+    k_dot(features, k_transpose(features))
+  }
+
+  # style loss computation
+  style_loss <- function(style, combination){
+    S <- gram_matrix(style)
+    C <- gram_matrix(combination)
+
+    channels <- 3
+    size <- img_nrows*img_ncols
+
+    k_sum(k_square(S - C)) / (4 * channels^2  * size^2)
+  }
+
+  # mantain the content of the base image
+  content_loss <- function(base, combination){
+    k_sum(k_square(combination - base))
+  }
+
+
+  # total variation loss, keeps the image locally coherent
+
+  total_variation_loss <- function(x){
+    y_ij  <- x[,1:(img_nrows - 1L), 1:(img_ncols - 1L),]
+    y_i1j <- x[,2:(img_nrows), 1:(img_ncols - 1L),]
+    y_ij1 <- x[,1:(img_nrows - 1L), 2:(img_ncols),]
+
+    a <- k_square(y_ij - y_i1j)
+    b <- k_square(y_ij - y_ij1)
+    k_sum(k_pow(a + b, 1.25))
+  }
+
+  eval_loss_and_grads <- function(image){
+    image <- array_reshape(image, c(1, img_nrows, img_ncols, 3))
+    outs <- f_outputs(list(image))
+    list(
+      loss_value = outs[[1]],
+      grad_values = array_reshape(outs[[2]], dim = length(outs[[2]]))
+    )
+  }
+  # Loss and gradients evaluator.
+  #
+  # This Evaluator class makes it possible
+  # to compute loss and gradients in one pass
+  # while retrieving them via two separate functions,
+  # "loss" and "grads". This is done because scipy.optimize
+  # requires separate functions for loss and gradients,
+  # but computing them separately would be inefficient.
+  Evaluator <- R6Class(
+    "Evaluator",
+    public = list(
+
+      loss_value = NULL,
+      grad_values = NULL,
+
+      initialize = function() {
+        self$loss_value <- NULL
+        self$grad_values <- NULL
+      },
+
+      loss = function(x){
+        loss_and_grad <- eval_loss_and_grads(x)
+        self$loss_value <- loss_and_grad$loss_value
+        self$grad_values <- loss_and_grad$grad_values
+        self$loss_value
+      },
+
+      grads = function(x){
+        grad_values <- self$grad_values
+        self$loss_value <- NULL
+        self$grad_values <- NULL
+        grad_values
+      }
+
+    )
+  )
+  
   
   whichpartrev <- function(x, n=5) {
     which(x >= -sort(-x, partial=n)[n])
@@ -507,6 +646,190 @@ server <- function(input, output, session) {
 
   
   
-}
+  #### elementos style transfer ####
+  
+  obtenerpathbase <- reactive({
+    #guardamos el path de la foto base
 
+    arc <- input$filebase
+    if(is.null(arc)) {
+      
+      return()}
+    else
+      # { print(gsub("\\\\", "/", arc$datapath))
+    {
+      imagebase <<- as.character(arc$datapath)
+      
+      return(arc$datapath)
+    }
+  }) 
+  
+  obtenerpathestilo <- reactive({
+    #guardamos el path de la foto estilo
+    
+    arc <- input$filestyle
+    if(is.null(arc)) {
+      
+      return()}
+    else
+      # { print(gsub("\\\\", "/", arc$datapath))
+    {
+      imagestyle <<- as.character(arc$datapath)
+      
+      return(arc$datapath)
+    }
+  })
+  output$iteraciones <- renderUI({
+    if(is.null(obtenerpathbase()) || is.null(obtenerpathestilo()))
+    {
+      return()
+    }
+    numericInput("iterations", "Number of iterations: ", value = 10)
+  })
+  output$botonempezartransfer <- renderUI({
+    if(is.null(obtenerpathbase()) || is.null(obtenerpathestilo()))
+    {
+      return()
+    }
+    actionButton("botonempezar", "Start style transfer!")
+  })
+  
+  output$imgbase <- renderPlot({
+    if (is.null(obtenerpathbase()))
+      {
+        return()
+    }
+    foto <- imager::load.image(imagebase)
+    plot(foto, main = "Base Image")
+  })
+  output$imgestilo <- renderPlot({
+    if (is.null(obtenerpathestilo()))
+    {
+      return()
+    }
+    foto <- imager::load.image(imagestyle)
+    plot(foto, main = "Style Reference")
+  })
+  
+  img_nrows <- 0
+  img_ncols <- 0
+  f_outputs <- NULL
+  #### observe event para style transfer ####
+  observeEvent(input$botonempezar, 
+               {
+                 if (input$iterations < 1)
+                 {
+                   return()
+                 }
+                 output$imgstyle <- NULL
+                 tensorflow::tf$compat$v1$disable_eager_execution()
+                 iterations <- input$iterations
+                 print(iterations)
+                 img <- image_load(imagebase)
+                 
+                 width <- img$size[[1]]
+                 height <- img$size[[2]]
+                 img_nrows <<- 400
+                 img_ncols <<- as.integer(width * img_nrows / height)
+                 
+                 # tensor representation of the images
+                
+                 base_image <- k_variable(preprocess_image(imagebase))
+                 style_reference_image <- k_variable(preprocess_image(imagestyle))
+                 
+                 # combination container
+                 
+                 combination_image <- k_placeholder(c(1, img_nrows, img_ncols, 3))
+                 
+                 # combine the 3 images into a single keras tensor
+                 # combine the 3 images into a single Keras tensor
+                 input_tensor <- k_concatenate(list(base_image, style_reference_image, 
+                                                    combination_image), axis = 1)
+                 
+                 # model creation
+                 
+                 model <- application_vgg16(input_tensor = input_tensor, weights = "pesos_vgg_impbar.h5", 
+                                            include_top = FALSE)
+                 
+                 nms <- map_chr(model$layers, ~.x$name)
+                 output_dict <- map(model$layers, ~.x$output) %>% set_names(nms)
+                 
+                 # combine these loss functions into a single scalar
+                 loss <- k_variable(0.0)
+                 layer_features <- output_dict$block4_conv2
+                 base_image_features <- layer_features[1,,,]
+                 combination_features <- layer_features[3,,,]
+                 
+                 loss <- loss + content_weight*content_loss(base_image_features, 
+                                                            combination_features)
+                 
+                 # which layers to use for features
+                 feature_layers = c('block1_conv1', 'block2_conv1',
+                                    'block3_conv1', 'block4_conv1',
+                                    'block5_conv1')
+                 
+                 
+                 for(layer_name in feature_layers){
+                   layer_features <- output_dict[[layer_name]]
+                   style_reference_features <- layer_features[2,,,]
+                   combination_features <- layer_features[3,,,]
+                   sl <- style_loss(style_reference_features, combination_features)
+                   loss <- loss + ((style_weight / length(feature_layers)) * sl)
+                 }
+                 
+                 loss <- loss + (total_variation_weight * total_variation_loss(combination_image))
+                 
+                 
+                 grads <- k_gradients(loss, combination_image)[[1]]
+                 
+                 f_outputs <<- k_function(list(combination_image), list(loss, grads))
+                 
+
+                 
+                 evaluator <- Evaluator$new()
+                 
+                 dms <- c(1, img_nrows, img_ncols, 3)
+                 x <- array(data = runif(prod(dms), min = 0, max = 255) - 128, dim = dms)
+                 
+                 # Run optimization (L-BFGS) over the pixels of the generated image
+                 # so as to minimize the loss
+                 im <- NULL
+                 imageaux <- NULL
+                # TODO: arreglar tiempo
+                 withProgress(message = "Generating combined picture", value=0, style = "notification",{
+                   for(i in 1:iterations){
+                     incProgress(0.1, detail = paste0("Iteration: ", i,"/", iterations))
+                     # Run L-BFGS
+                     opt <- optim(
+                       array_reshape(x, dim = length(x)), fn = evaluator$loss, gr = evaluator$grads, 
+                       method = "L-BFGS-B",
+                       control = list(maxit = 15)
+                     )
+                     
+                     # Print loss value
+                     # print(opt$value)
+                     
+                     # decode the image
+                     imageaux <- x <- opt$par
+                     
+                     ## TODO: MIRAR FALLOS A PARTIR DE AQUÍ: cannot reshape array of size 1 into shape (1, 400, 319, 3) EN LA SIGUIENTE LINEA
+                     imageaux <- array_reshape(imageaux, dms)
+                     
+                     # png(paste0("styleprueba/", i, ".png"))
+                     # im <- deprocess_image(image)
+                     # plot(as.raster(im))
+                     # dev.off()
+                     
+                   }
+                   
+                 } )
+                 
+                 im <- deprocess_image(imageaux)
+                 k_clear_session()
+                 output$imgstyle <- renderPlot({
+                   plot(as.raster(im), main = "Result")
+                 })
+                 })
+ 
+}
 shinyApp(ui, server)
